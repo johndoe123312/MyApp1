8f84da602bcf903d153293eb5bf5ca0d7e928d0a
John Doe <203684040+johndoe123312@users.noreply.github.com>
2025-03-23 14:39:59 +0900

    My tasks are done :)

--- MyApp1/AppDelegate.swift
+++ MyApp1/AppDelegate.swift
@@ -1,8 +1,7 @@
-//
 //  AppDelegate.swift
 //  MyApp1
 //
-//  Created by John Doe on 23.03.2025.
+//  Created by John Doe on 22.03.2025.
 //
 
 import UIKit
@@ -13,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print("2 didFinishLaunchingWithOptions")
         return true
     }
 
@@ -31,6 +30,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_: UIApplication, willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool{
+        print("1 willFinishLaunchingWithOptions")
+        return true
+    }
+    
+    
+    func applicationDidBecomeActive(_ application: UIApplication){
+            print("applicationDidBecomeActive")
+    }
+    
+    func applicationDidEnterBackground(_: UIApplication) {
+            // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+            // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+            print(33)
+    }
+        
+        func applicationWillEnterForeground(_: UIApplication) {
+            // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+            print(44)
+        }
+        
+        func applicationWillResignActive(_: UIApplication) {
+            // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+            // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+            print(55)
+        }
+    
+        func applicationWillTerminate(_: UIApplication) {
+            // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+            print(66)
+        }
+        
 }
 
+
+// имплементация ниже подлежащих методов
+
+//ok application:willFinishLaunchingWithOptions: — Этот метод вашего приложения позволяет вам выполнять код во время запуска приложения.
+//ok application:didFinishLaunchingWithOptions: — Этот метод позволяет вам перед окончанием запуска выполнить код прежде чем показать ваше приложение пользователю.
+//ok applicationDidBecomeActive: — Дает вам знать, что оно становятся приложением переднего плана. Используйте этот метод для последних приготовлений.
+//ok applicationWillResignActive: — Дает вам знать, что приложение уходит из режима переднего плана. Используйте этот метод для помещения вашего приложения в режим покоя.
+//ok applicationDidEnterBackground: — Дает вам знать, что приложение запущено в фоне и может быть выключено в любое время.
+//applicationWillEnterForeground: — Дает вам знать, что ваше приложение перемещено из фона обратно на передний план, но то, что оно еще не активно.
+//applicationWillTerminate: — Дает вам знать, что приложение было выключено. Этот метод не вызывается, если приложение было в состоянии suspended(приостановлено)
--- MyApp1/SceneDelegate.swift
+++ MyApp1/SceneDelegate.swift
@@ -1,8 +1,7 @@
-//
 //  SceneDelegate.swift
 //  MyApp1
 //
-//  Created by John Doe on 23.03.2025.
+//  Created by John Doe on 22.03.2025.
 //
 
 import UIKit
@@ -29,24 +28,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print(" 444 sceneDidBecomeActive")
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print(" 444 sceneWillResignActive")
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        print(" 555 sceneWillResignActive")
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        print(" 666 func sceneDidEnterBackground")
     }
 
 
 }
-
